da307b6ea644907dd20ae4e617760681f6a4a687
Travis <travis.m.bruce@gmail.com>
2023-11-03 14:05:38 -0700

    Add redux toolkit store

---
+++ ChatApp/screens/Threads.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Threads = () => {
+  return (
+    <View>
+      <Text>Threads</Text>
+    </View>
+  );
+};
+
+export default Threads;
+
+const styles = StyleSheet.create({});
