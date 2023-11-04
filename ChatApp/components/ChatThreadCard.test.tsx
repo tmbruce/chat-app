a3b9de568d197aece7b285f826701e5c6d677beb
Travis <travis.m.bruce@gmail.com>
2023-11-03 21:22:15 -0700

    Set up initial testing fro ChatThreadCard component

--- ChatApp/components/ChatThreadCard.test.tsx
+++ ChatApp/components/ChatThreadCard.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import {render} from '@testing-library/react-native';
+// ChatThreadCard.test.tsx
 
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
 import ChatThreadCard from './ChatThreadCard';
+import {useNavigation} from '@react-navigation/native';
+
+// Mocking the useNavigation hook
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
 
 describe('ChatThreadCard', () => {
+  const navigation = {
+    navigate: jest.fn(),
+  };
+
+  // Set up the mock useNavigation hook
+  beforeEach(() => {
+    (useNavigation as jest.Mock).mockReturnValue(navigation);
+  });
+
   const chatThread = {
     threadId: '1',
     name: 'Jerry Garcia',
@@ -14,12 +32,25 @@ describe('ChatThreadCard', () => {
 
   test('renders correctly', () => {
     const {getByText} = render(<ChatThreadCard chatThread={chatThread} />);
-    expect(getByText(chatThread.name)).toBeTruthy();
-    expect(getByText(chatThread.message)).toBeTruthy();
-    expect(getByText(chatThread.date)).toBeTruthy();
 
+    const nameElement = getByText(chatThread.name);
+    const messageElement = getByText(chatThread.message);
+    const dateElement = getByText(chatThread.date);
+
+    expect(nameElement).toBeTruthy();
+    expect(messageElement).toBeTruthy();
+    expect(dateElement).toBeTruthy();
+  });
+
+  test.skip('navigates to thread screen on press', () => {
     const {getByTestId} = render(<ChatThreadCard chatThread={chatThread} />);
-    const isMessageRead = getByTestId('messageRead-field');
-    expect(isMessageRead).toBeTruthy();
+    const touchable = getByTestId('touchable');
+
+    fireEvent.press(touchable);
+
+    expect(navigation.navigate).toHaveBeenCalledWith('ThreadScreen', {
+      threadId: chatThread.threadId,
+      threadName: chatThread.name,
+    });
   });
 });
